63ef60b75dc71f9061d524207a74536ea7dc281f
Elizabethucar <elizg77@hotmail.com>
2022-10-20 08:56:35 +0200

    routes added

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 4000
+const app = express()
+
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(PORT,()=>{
+  (`listening on port ${PORT}`)
+})
