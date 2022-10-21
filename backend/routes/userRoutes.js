0cff44f55c6f0a3705e1b8d39a8880a7e4647d03
Elizabethucar <elizg77@hotmail.com>
2022-10-21 08:59:06 +0200

    jwt and bcrypt installed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
-const express= require('express')
-const router= express.Router()
-const {registerUser} = require('../controller/userController')
+const express = require('express')
+const router = express.Router()
+const {registerUser,loginUser,getOneUser} = require('../controller/userController')
 
 router.post('/', registerUser)
+router.post('/login', loginUser)
+router.get('/getOne', getOneUser)
 
 module.exports= router
