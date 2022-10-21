6a6d8c77b08b4829a4a71abb6cb4fbfee7b79cfe
Elizabethucar <elizg77@hotmail.com>
2022-10-21 14:37:28 +0200

    jwt working

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -30,7 +30,8 @@ const registerUser = asyncHandler(async (req,res)=>{
   res.status(201).json({
     _id: user.id,
     name: user.name,
-    email: user.email
+    email: user.email,
+    token: generateToken(user._id)
   })
  }else{
   res.status(400)
@@ -39,12 +40,34 @@ const registerUser = asyncHandler(async (req,res)=>{
 })
 
 const loginUser =asyncHandler (async (req,res)=>{
+  const {email, password} = req.body
+  //check for email
+  const user = await User.findOne({email})
+    if(user && (await bcrypt.compare(password, user.password))) {
+     res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,  
+      token: generateToken(user._id)
+     }) 
+    }else{
+      res.status(400)
+      throw new Error('Invalid credentials')
+    }
+
   res.json({message: 'login user'})
 })
 
 const getOneUser = asyncHandler (async(req,res)=>{
   res.json({message: 'get one user'})
 })
+
+//generate JWT token
+const generateToken =(id)=>{
+  return jwt.sign({id},process.env.JWT_SECRET,{
+    expiresIn: '30d',
+  } )
+}
 
 module.exports = {
   registerUser,
