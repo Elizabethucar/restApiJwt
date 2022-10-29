ebc304a83b885a8eee8fae0941340df16a8ee98d
Elizabethucar <elizg77@hotmail.com>
2022-10-29 15:50:16 +0200

    frontend-react created

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_DB_URI)
+    
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
     console.log(error);
