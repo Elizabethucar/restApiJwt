a4c96c6f06d21d87a6b6eaac8a915bec7d1a523f
Elizabethucar <elizg77@hotmail.com>
2022-10-30 14:45:31 +0100

    goal form added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -59,12 +59,7 @@ const loginUser =asyncHandler (async (req,res)=>{
 })
 //private route
 const getOneUser = asyncHandler (async(req,res)=>{
- const {_id,name,email}= await User.findById(req.user.id)
- res.status(200).json({
-  id: _id,
-  name,
-  email,
- })
+res.status(200).json(req.user)
 })
 
 //generate JWT token
